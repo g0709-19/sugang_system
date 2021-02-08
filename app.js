f80572b74929fafbcd853a644b199a941e816269
Jun <sch030518@gmail.com>
2021-02-08 20:58:11 +0900

    static 연결

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.send('Hello World! 안녕하세요!');
 });
 
 app.listen(PORT, () => {
     console.log(`${PORT}번 포트로 열림`);
-})
+});
