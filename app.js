d2cfa80280aa152af9a4df472bfacfc53c29f289
gxtxhm <macdollyh@naver.com>
2021-02-09 17:38:05 +0900

    로그인,로그아웃기능 추가

--- app.js
+++ app.js
@@ -4,9 +4,23 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
+let bodyParser = require("body-parser");
+let msg = require ('dialog')//경고창 띄워주는 모듈
+
+let mysql      = require('mysql');
+let db = mysql.createConnection({
+  host     : '222.118.77.5',
+  user     : 'gxtxhm',
+  password : 'qwer1234@',
+  database : 'sugang' 
+});
+ 
+db.connect();
+
 app.use(express.static('public'));
 app.set('views','./views');//views폴더가 있어야한다. 
 app.set('view engine','jade');//views폴더 안에 jade 확장자에 대해 설정하였다.
+app.use(bodyParser.urlencoded({extended:false}));//bodyparser설정
 
 app.get('/', (req, res) => {
     // 로그인 페이지 연결
@@ -18,8 +32,29 @@ app.get('/', (req, res) => {
     })
 });
 
-app.get('/1',(req,res)=>{
-    res.render("index");
+app.post('/login',(req,res)=>{
+    
+    db.query('SELECT * FROM user where id=? and password=?',[req.body.Id,req.body.Password],(err,result)=>{
+
+        if (err){
+            res.status(500).send('Internal server error!');
+            console.log(error);
+        }
+        console.log(result);
+        if(result.length>0)//로그인완료되면
+        {
+            res.render("index",{userInfo:`${result[0].name}(${result[0].id})`});
+        }
+        else//실패하면 로그인화면으로
+        {
+            msg.info ("로그인에 실패하였습니다!");  
+            res.redirect("/");
+        }
+
+    })
+
+    
+    
 })
 
 app.listen(PORT, () => {
